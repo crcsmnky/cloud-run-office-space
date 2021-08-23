0d87a08594c6d98c8a13eec142b8dbcfce09b463
Sandeep Parikh <sandeep@clusterbeep.org>
2021-08-23 17:40:43 -0500

    support for pubsub push and redis caching

--- account-balance/balance.py
+++ account-balance/balance.py
@@ -1,5 +1,7 @@
 import os
-import pymongo
+import base64
+import redis 
+import json
 
 from flask import Flask, request, render_template
 from google.cloud import firestore
@@ -11,21 +13,32 @@ db = firestore.Client()
 
 ACCT = os.environ.get('ACCT_COLLECTION', 'account')
 
-@app.route("/", methods=['GET'])
-def balance():
-    txn_stream = db.collection(ACCT).order_by(
-        'date', direction=firestore.Query.DESCENDING).stream()
+rhost = os.environ.get('REDIS_HOST', 'localhost')
+rport = int(os.environ.get('REDIS_PORT', 6379))
+cache = redis.Redis(host=rhost, port=rport) 
 
-    total = 0
+
+@app.route("/return-balance", methods=['GET'])
+def balance():
+    total = float(cache.get('total-balance'))
     txns = list()
+    txn_stream = db.collection(ACCT).order_by(
+        'date', direction=firestore.Query.DESCENDING).limit(10).stream()
 
     for txn in txn_stream:
         txns.append(txn.to_dict())
-        total += txn.get('amount')
 
     return render_template('balance.html', txns=txns, total=total)
 
 
+@app.route("/update-balance", methods=["POST"])
+def update_balance():
+    msg = json.loads(request.form['message'])
+    amount = float(base64.b64decode(msg['data']).decode('utf-8').strip())
+    cache.incrbyfloat('total-balance', amount)
+    return 'ok', 200
+
+
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('FLASK_PORT', 5000))
     app.run(host='127.0.0.1', port=port, debug=False)

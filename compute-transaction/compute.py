cfb4fa58d4f516b0e31da2179c41558d6e36bb18
Sandeep Parikh <sandeep@clusterbeep.org>
2021-08-23 17:41:47 -0500

    support for sending amounts via pubsub

--- compute-transaction/compute.py
+++ compute-transaction/compute.py
@@ -5,6 +5,7 @@ from datetime import datetime
 from flask import Flask, request
 
 from google.cloud import firestore
+from google.cloud import pubsub_v1 as pubsub
 
 app = Flask(__name__)
 app.secret_key = "7ad6b29a8134b52f54a96e276d38c7f1" # md5 -s "office space"
@@ -13,6 +14,12 @@ db = firestore.Client()
 
 BANK = os.environ.get('BANK_COLLECTION', 'holdings')
 ACCT = os.environ.get('ACCT_COLLECTION', 'account')
+
+PROJECT = os.environ.get('PROJECT_ID')
+TOPIC = os.environ.get('TOPIC_ID')
+
+publisher = pubsub.PublisherClient()
+topic_path = publisher.topic_path(PROJECT, TOPIC)
 
 @app.route('/compute', methods=['POST'])
 def compute():
@@ -41,11 +48,14 @@ def compute():
     }
     db.collection(ACCT).add(accttxn)
 
+    msg = str(rem).encode('utf-8')
+    future = publisher.publish(topic_path, msg)
+
     return 'added', 201
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 6000))
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='127.0.0.1', port=port, debug=False)
 
 

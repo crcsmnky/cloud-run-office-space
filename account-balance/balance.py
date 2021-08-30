6183eb3797d9f6211c2a0cefe1db4401c2c3034e
Sandeep Parikh <sandeep@clusterbeep.org>
2021-08-30 16:39:22 -0500

    changed listening address

--- account-balance/balance.py
+++ account-balance/balance.py
@@ -41,4 +41,4 @@ def update_balance():
 
 if __name__ == '__main__':
     port = int(os.environ.get('FLASK_PORT', 5000))
-    app.run(host='127.0.0.1', port=port, debug=False)
+    app.run(host='0.0.0.0', port=port, debug=False)
--- compute-transaction/compute.py
+++ compute-transaction/compute.py
@@ -56,7 +56,7 @@ def compute():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='127.0.0.1', port=port, debug=False)
+    app.run(host='0.0.0.0', port=port, debug=False)
 
 
 
